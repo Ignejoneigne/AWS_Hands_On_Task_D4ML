6393800b7c68fa11fce0796e55e51e84216ae925
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-23 15:19:47 +0300

    Update user_data.sh, update terraform version

--- user_data.sh
+++ user_data.sh
@@ -19,8 +19,8 @@ sudo apt update
 sudo apt install -y unzip git
 
 # Download and install Terraform (latest version)
-sudo wget https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_amd64.zip
-unzip terraform_1.0.0_linux_amd64.zip
+sudo wget https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_amd64.zip
+unzip terraform_1.6.2_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
 # Install the AWS CLI
